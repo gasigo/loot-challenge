cbc9f22cc8317d0e4c0b57210426824c7dc9e29e
Gabriel Gomes <gasigo10@gmail.com>
2019-01-13 14:03:32 +0000

    feat(network): add abstraction for http verbs
    add HTTPMethod enum to abstract the HTTP verbs.

---
+++ Loot Challenge/Network/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  Loot Challenge
+//
+//  Created by Gabriel Gomes on 13/01/19.
+//  Copyright © 2019 GDev. All rights reserved.
+//
+
+enum HTTPMethod: String {
+	case get = "GET"
+	case post = "POST"
+	case put = "PUT"
+	case delete = "DELETE"
+}
